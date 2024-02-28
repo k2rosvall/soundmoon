60d013cca01659ddad40cd1ca89596d2cf768f21
Karla Rosas <k2.rosas.v@gmail.com>
2024-02-27 19:33:27 -0600

    Add update and edit methods for profile controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,9 +4,23 @@ class ProfilesController < ApplicationController
   before_action :set_profile
   def show; end
 
+  def edit; end
+
+  def update
+    if @profile.update(profile_params)
+      redirect_to profile_path, notice: "Profile updated successfully"
+    else
+      render :edit, :unprocessable_entitys
+    end
+  end
+
   private
 
   def set_profile
     @profile = current_user.profile
+  end
+
+  def profile_params
+    params.require(:profile).permit(:username, :first_name, :last_name)
   end
 end
